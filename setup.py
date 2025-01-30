3914214cccfec4583fb4a0a1320d484dcfdbaa18
William Yang <williamyang2017@gmail.com>
2025-01-31 01:42:13 +1100

    Only install openEMS and CSXCAD from wheel for windows

--- setup.py
+++ setup.py
@@ -2,33 +2,53 @@ from setuptools import setup, find_packages
 import os
 import pathlib
 
+requirements = [
+    "numpy",
+    "matplotlib",
+    "coloredlogs",
+    "nanomesh",
+    "Pillow",
+    "scikit-rf",
+    "h5py",
+]
+
 PYTHON_VERSION = sys.version_info[:2]
-OPENEMS_ROOT = os.path.abspath("./vendor/openems/openEMS/")
-OPENEMS_WHEEL_FILEPATHS = {
-    (3, 10): os.path.join(OPENEMS_ROOT, "python", "openEMS-0.0.36-cp310-cp310-win_amd64.whl"),
-    (3, 11): os.path.join(OPENEMS_ROOT, "python", "openEMS-0.0.36-cp311-cp311-win_amd64.whl"),
-}
-CSXCAD_WHEEL_FILEPATHS = {
-    (3, 10): os.path.join(OPENEMS_ROOT, "python", "CSXCAD-0.6.3-cp310-cp310-win_amd64.whl"),
-    (3, 11): os.path.join(OPENEMS_ROOT, "python", "CSXCAD-0.6.3-cp311-cp311-win_amd64.whl"),
-}
 
-def get_wheel(name, filepaths, version):
-    filepath = filepaths.get(version, None)
-    if filepath is None:
-        format_version = lambda v: '.'.join(map(str, v))
-        current_version = format_version(PYTHON_VERSION)
-        supported_versions = [format_version(v) for v in filepaths.keys()]
-        raise RuntimeError(
-            f"Unsupported Python version {current_version} for wheel {name}. "
-            f"Supported versions are: [{','.join(supported_versions)}]."
-        )
-    return filepath
+if os.name == 'nt':
+    ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
+    OPENEMS_ROOT = os.path.join(ROOT_DIR, "vendor/openems/openEMS/")
+    OPENEMS_ROOT = os.path.abspath(OPENEMS_ROOT)
+    OPENEMS_WHEEL_FILEPATHS = {
+        (3, 10): os.path.join(OPENEMS_ROOT, "python", "openEMS-0.0.36-cp310-cp310-win_amd64.whl"),
+        (3, 11): os.path.join(OPENEMS_ROOT, "python", "openEMS-0.0.36-cp311-cp311-win_amd64.whl"),
+    }
+    CSXCAD_WHEEL_FILEPATHS = {
+        (3, 10): os.path.join(OPENEMS_ROOT, "python", "CSXCAD-0.6.3-cp310-cp310-win_amd64.whl"),
+        (3, 11): os.path.join(OPENEMS_ROOT, "python", "CSXCAD-0.6.3-cp311-cp311-win_amd64.whl"),
+    }
 
-OPENEMS_WHEEL_FILEPATH = get_wheel("openEMS", OPENEMS_WHEEL_FILEPATHS, PYTHON_VERSION)
-CSXCAD_WHEEL_FILEPATH = get_wheel("CSXCAD", CSXCAD_WHEEL_FILEPATHS, PYTHON_VERSION)
-OPENEMS_WHEEL_URI = pathlib.Path(OPENEMS_WHEEL_FILEPATH).as_uri()
-CSXCAD_WHEEL_URI = pathlib.Path(CSXCAD_WHEEL_FILEPATH).as_uri()
+    def get_wheel(name, filepaths, version):
+        filepath = filepaths.get(version, None)
+        if filepath is None:
+            format_version = lambda v: '.'.join(map(str, v))
+            current_version = format_version(PYTHON_VERSION)
+            supported_versions = [format_version(v) for v in filepaths.keys()]
+            raise RuntimeError(
+                f"Unsupported Python version {current_version} for wheel {name}. "
+                f"Supported versions are: [{','.join(supported_versions)}]."
+            )
+        return filepath
+
+    OPENEMS_WHEEL_FILEPATH = get_wheel("openEMS", OPENEMS_WHEEL_FILEPATHS, PYTHON_VERSION)
+    CSXCAD_WHEEL_FILEPATH = get_wheel("CSXCAD", CSXCAD_WHEEL_FILEPATHS, PYTHON_VERSION)
+    OPENEMS_WHEEL_URI = pathlib.Path(OPENEMS_WHEEL_FILEPATH).as_uri()
+    CSXCAD_WHEEL_URI = pathlib.Path(CSXCAD_WHEEL_FILEPATH).as_uri()
+
+    windows_requirements = [
+        f"openEMS @ {OPENEMS_WHEEL_URI}",
+        f"CSXCAD @ {CSXCAD_WHEEL_URI}",
+    ]
+    requirements.extend(windows_requirements)
 
 setup(
     name="gerber2ems",
@@ -44,15 +64,5 @@ setup(
             "gerber2ems = gerber2ems.main:main"
         ]
     },
-    install_requires=[
-        f"openEMS @ {OPENEMS_WHEEL_URI}",
-        f"CSXCAD @ {CSXCAD_WHEEL_URI}",
-        "numpy",
-        "matplotlib",
-        "coloredlogs",
-        "nanomesh",
-        "Pillow",
-        "scikit-rf",
-        "h5py",
-    ]
+    install_requires=requirements,
 )
